d9d568b81be3d3c7bcb2aff4273382295c00d066
singh-that-codes <japjeets2003@gmail.com>
2024-02-17 23:12:45 +0530

    Applied dev 2

---
+++ src/main/java/com/example/MyServlet.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import java.io.IOException;
+
+public class MyServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        response.getWriter().println("<html><body><h1>Hello, World!</h1></body></html>");
+    }
+}
+
